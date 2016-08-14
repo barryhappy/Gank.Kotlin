a920f7571e4a2342ccf9de306d3b8a4f124a0744
张新强 <191988637@qq.com>
2016-08-15 00:29:43 +0800

    优化『关于页面』，使用webView进行显示

--- app/src/main/java/com/barryzhang/gankkotlin/ui/AboutActivity.kt
+++ app/src/main/java/com/barryzhang/gankkotlin/ui/AboutActivity.kt
@@ -1,31 +1,32 @@
 package com.barryzhang.gankkotlin.ui
 
-import android.support.design.widget.FloatingActionButton
+import android.webkit.WebView
 import butterknife.BindView
 import com.barryzhang.gankkotlin.R
-import com.barryzhang.gankkotlin.ui.base.BaseActivity
-import us.feras.mdv.MarkdownView
+import com.barryzhang.gankkotlin.ui.base.BaseAppBarActivity
 
 /**
  * https://github.com/barryhappy
  * Created by Barry on 16/8/8
  */
 
-class AboutActivity : BaseActivity(){
-    override fun getLayoutResourceID() = R.layout.activity_about
+class AboutActivity : BaseAppBarActivity(){
+    override fun getLayoutResourceID() = R.layout.content_about
 
-    @BindView(R.id.markDownView)
-    lateinit var markdownView: MarkdownView
-    @BindView(R.id.fab)
-    lateinit var fab: FloatingActionButton
+    @BindView(R.id.webView)
+    lateinit var webView: WebView
 
     override fun afterInject() {
 
     }
 
     override fun start() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener { back() }
+        title = "关于"
 
-        markdownView.loadMarkdownFile("file:///android_asset/about.md")
+        webView.loadUrl("file:///android_asset/about.html" )
     }
 
 }
